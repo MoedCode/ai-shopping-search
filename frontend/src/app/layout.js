dd37252014a7ecf1db2aa533e39440447a9b0a58
Mohamed Mahmoud <pro-eng@DESKTOP-S296B4S.localdomain>
2026-01-26 01:16:11 +0200

    Mon Jan 26 01:16:11 EET 2026

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -9,7 +9,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
       <body>
+        <h3 className="text-3xl font-bold text-center mb-4">ElA-shba7 Shopping Agent</h3>
         {children}
+
       </body>
     </html>
   );
